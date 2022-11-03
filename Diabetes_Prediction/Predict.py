6b4d84bef36f5bf6fe361c9a0852ef4d002be221
maclavijo <clavijomarioa@outlook.com>
2022-11-03 14:43:13 -0500

    Diabetes - Fixing bus streamlit app

--- Diabetes_Prediction/Predict.py
+++ Diabetes_Prediction/Predict.py
@@ -95,7 +95,7 @@ def predict(inputdata):
     for file in files:
         filename = file.split('.')[0]
         print(os.listdir)
-        with open(file, 'rb') as f:
+        with open('/' +file, 'rb') as f:
             models[filename] = pickle.load(f)        
 
     # Make predictions
